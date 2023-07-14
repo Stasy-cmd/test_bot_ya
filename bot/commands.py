7956e5462db131c48c4de968619ab797497e100e
Stasy23 <anastasia.marochkina@kvint.io>
2023-07-14 20:30:57 +0200

    Правки, связанные со структурой проекта.

--- bot/commands.py
+++ bot/commands.py
@@ -57,7 +57,7 @@ async def next_command(message: types.Message):
 
 
 async def photo_command(message: types.Message):
-    photo = open("../data/school.jpg", "rb")
+    photo = open("data/school.jpg", "rb")
     caption = emoji.emojize(
         "А это я в 11-м классе со своей\n"
         "любимой учительницей:smiling_face_with_sunglasses:"
@@ -66,25 +66,25 @@ async def photo_command(message: types.Message):
 
 
 async def gpt_command(message: types.Message):
-    with open("../data/gpt.m4a", mode="rb") as file:
+    with open("data/gpt.m4a", mode="rb") as file:
         binary_content = file.read()
     await message.answer_voice(binary_content, reply_markup=get_button_listen())
 
 
 async def bd_command(message: types.Message):
-    with open("../data/db.m4a", mode="rb") as file:
+    with open("data/db.m4a", mode="rb") as file:
         binary_content = file.read()
     await message.answer_voice(binary_content, reply_markup=get_button_listen())
 
 
 async def love_command(message: types.Message):
-    with open("../data/love.m4a", mode="rb") as file:
+    with open("data/love.m4a", mode="rb") as file:
         binary_content = file.read()
     await message.answer_voice(binary_content, reply_markup=get_button_listen())
 
 
 async def self_command(message: types.Message):
-    photo = open("../data/self.png", "rb")
+    photo = open("data/self.png", "rb")
     caption = emoji.emojize("Привет-привет :victory_hand:")
     await message.answer_photo(photo, caption=caption, reply_markup=get_button_views())
 
@@ -97,11 +97,11 @@ async def url_command(message: types.Message):
 async def post_command(message: types.Message):
     text = emoji.emojize(get_post())
     media = types.MediaGroup()
-    media.attach_photo(types.InputFile("../data/post/1.png"), text)
-    media.attach_photo(types.InputFile("../data/post/2.jpg"))
-    media.attach_photo(types.InputFile("../data/post/3.png"))
-    media.attach_photo(types.InputFile("../data/post/4.jpg"))
-    media.attach_video(types.InputFile("../data/post/video.MOV"))
+    media.attach_photo(types.InputFile("data/post/1.png"), text)
+    media.attach_photo(types.InputFile("data/post/2.jpg"))
+    media.attach_photo(types.InputFile("data/post/3.png"))
+    media.attach_photo(types.InputFile("data/post/4.jpg"))
+    media.attach_video(types.InputFile("data/post/video.MOV"))
     await message.answer_media_group(media=media)
     await message.answer(
         "<b>Главное меню</b>", parse_mode="HTML", reply_markup=get_button_all()
